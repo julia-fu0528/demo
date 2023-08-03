75a9c96bdbfab6e889809c96963993b6ba98abf9
Julia Fu <wanjia_fu@brown.edu>
2023-08-03 17:38:55 -0400

    two canvas

---
+++ vite-project/js/pers-canvas.js
@@ -0,0 +1,35 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#ffffff');
+const sizes = {
+    width: 600,
+    height: 350,
+}
+
+const geometry = new THREE.SphereGeometry(3, 64, 64)
+const material = new THREE.MeshStandardMaterial({
+    color: '#ff0083',
+    roughness: 0.3,
+})
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh)
+
+const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 100);
+camera.position.set(0, 20, 0)
+camera.lookAt(0,0, 0)
+scene.add(camera)
+
+const light = new THREE.PointLight(0xffffff,1, 100 );
+light.position.set(0, 10, 10)
+light.intensity = 1
+scene.add(light)
+
+const gridHelper = new THREE.GridHelper(30);
+scene.add(gridHelper);
+
+const canvas = document.querySelector('.pers-canvas');
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(1);
+renderer.render(scene, camera);
