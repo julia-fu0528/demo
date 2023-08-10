8ec1836520b6af73ce327daa328c7c7b34a5ba14
Julia Fu <wanjia_fu@brown.edu>
2023-08-10 17:49:20 -0400

    dashed lines

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -4,8 +4,8 @@ import * as THREE from 'three';
 // import * as np from 'numpy';
 import * as CAM from './canvas/cam-canvas.js';
 import * as PERS from './canvas/pers-canvas.js';
-import * as WORLD from './textblocks/world-points.js'
-import * as INTRINSICS from './textblocks/intrinsics.js'
+import * as WORLD from './textboxes/world-points.js'
+import * as INTRINSICS from './textboxes/intrinsics.js'
 import * as RENDER from './render.js'
 import { ZeroCurvatureEnding } from 'three';
 
